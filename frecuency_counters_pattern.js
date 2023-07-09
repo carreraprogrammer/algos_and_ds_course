538e3312a9d27e1935f14e3276e50f9c5f5ee685
Daniel <carreraprogrammer@gmail.com>
2023-07-09 17:35:47 -0500

    Add big O(N) solution

--- frecuency_counters_pattern.js
+++ frecuency_counters_pattern.js
@@ -76,3 +76,29 @@ function same(arr1, arr2){
  console.log(same([1, 2, 3], [1, 4, 9])) // true
  console.log(same([1, 2, 3], [1, 4, 9, 1])) // false
  console.log(same([1, 2, 3], [9, 4, 1]))
+
+
+ // O(n)
+
+ function same2(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for(let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+    }
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    return true
+}
