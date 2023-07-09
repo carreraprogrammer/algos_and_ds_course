c15ac287363224659f8000f4f7ce810062ede873
Daniel <carreraprogrammer@gmail.com>
2023-07-09 17:17:59 -0500

    First solution

--- frecuency_counters_pattern.js
+++ frecuency_counters_pattern.js
@@ -33,3 +33,56 @@ same([1,2,1], [4,4,1]) // false (must be same frequency) */
 
 // EXPLORE EXAMPLES
 
+// Simple examples
+
+/* same([1], [1]) // true
+same([1], [1, 1]) // false (Not the same frecuency)
+same([1, 2, 3], [1, 4, 9]) // true
+same([1, 2, 3], [1, 4, 9, 1]) // false
+
+// Empty inputs
+
+same([], [1]) // error
+
+// invalid input
+
+same([1, 2, 3], "This is a string!!") // error */
+
+// BREAK IT DOWN
+
+// If arguments are arrays and every element inside the array is an integer
+  // Compare elements from the first array with all the elements in the second array
+  // If it match with his square in the second array
+    // Delete the pair
+  // If at the end of the loop, the two arrays are empty
+  // return true
+  // else
+  // return false
+
+const same = (firstArray, secondArray) => {
+    if (Array.isArray(firstArray) && Array.isArray(secondArray)) {
+      let matchFirst = []
+      let matchSecond = []
+
+      firstArray.forEach(
+        function(number) {
+          for(let i = 0; i < secondArray.length; i ++) {
+            if(number ** 2 === secondArray[i]) {
+              matchFirst.push(number)
+              matchSecond.push(secondArray[i])
+            }
+          }
+        }
+      )
+
+      if (firstArray.join('') == matchFirst.join('') && secondArray.join('') == matchSecond.join('')) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      throw new Error("Please, insert arrays as arguments !");
+    }
+  };  
+
+ console.log(same([2, 3, 6], [4, 9, 32]))
