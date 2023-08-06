f485fba625fe44f7e917fdc56aee1f4c6e7cb459
Daniel <carreraprogrammer@gmail.com>
2023-08-06 18:03:41 -0500

    Add js solution for product of array problem

---
+++ product-of-array.js
@@ -0,0 +1,8 @@
+/* Write a function called productOfArray which takes in an array of numbers and returns the product of them all. */
+let productOfArray = (arr) => {
+  if (arr.length === 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1))
+}
+
+console.log(productOfArray([1,2,3])) // 6
+console.log(productOfArray([1,2,3,10])) // 60
