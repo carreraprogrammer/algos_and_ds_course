b4e4e5080b014880aac0bd7437e3f9de25bf500d
Daniel <carreraprogrammer@gmail.com>
2023-08-26 21:40:38 -0500

    Add solution for naive string search on js

--- naive-string-search.js
+++ naive-string-search.js
@@ -1,12 +1,22 @@
-function naiveSearch(long, short){
-  var count = 0;
-  for(var i = 0; i < long.length; i++){
-      for(var j = 0; j < short.length; j++){
-         if(short[j] !== long[i+j]) break;
-         if(j === short.length - 1) count++;
-      }
+let naiveSearch= (longer, short) =>{
+  let counter = 0;
+  let j = 0
+  let i = 0;
+
+  while(j <= longer.length) {
+   if(i === short.length) {
+    counter ++
+    i = 0;
+   } 
+   if(longer[j] == short[i]) {
+    j ++
+    i ++
+   } else if(longer[j] != short[i]){
+    j ++
+   }
   }
-  return count;
+
+  return counter;
 }
 
-naiveSearch("lorie loled", "lol")
+console.log(naiveSearch("loler roler lole", "ole"))// 3
