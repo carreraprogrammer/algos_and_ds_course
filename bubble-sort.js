6e2d0eb261dd0260c5982bbef1f9767eae617c69
Daniel <carreraprogrammer@gmail.com>
2023-09-08 13:36:20 -0500

    Add bubble sort solution

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,19 +1,13 @@
-function bubbleSort(arr) {
-  let i = 0;
-  let j = 1;
-  let counter = 0;
-
-  while(counter < arr.length) {
-    if(arr[i] < arr[j]){
-      i++
-      j++
-    } else if(arr[i] > arr[j]) {
-      [arr[i], arr[j]] = [arr[j], arr[i]]
+let bubbleSort = (arr) => {
+  for(let i = arr.length - 1; i >= 0; i ++) {
+    for(let j = 0; j < i -1; j ++) {
+      if(arr[j] > arr[j+1]){
+        [arr[j], arr[j + 1]] = [arr[j+1], arr[j]]
+      }
     }
-    counter ++
   }
-  
   return arr
 }
+
 
 console.log(bubbleSort([1,4,3,2,5,8,9,7]))
