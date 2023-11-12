b53a2a28076b4799673da25cf2b63bcd1410723a
Daniel <carreraprogrammer@gmail.com>
2023-11-12 09:43:54 -0500

    Refactor minSubArrayLen function to improve readability and efficiency

--- problem solving patterns/min-subarray-len.js
+++ problem solving patterns/min-subarray-len.js
@@ -1,38 +1,32 @@
 function minSubArrayLen(nums, sum) {
-// [1, 2, 3, 4, 5], 9
-//        i  j
-// los dos indexes empiezan en la misma posicion
-// mi total va a sumar cada numero del arr hasta que total < sum, usando el index end siempre y cuando total sea < sum
-// si mi total es >= sum, significa que puedo reducir la brecha y aumentar mi start para buscar un array mas pequeno
-// cuando mi total es >= sum, voy a guardar el length en minLen, que es la resta de mi end - start
-// este proceso se repite indefinidamente hasta que mi start cubra el arr length
-// en cada paso se compara el valor minimo entre mi current minLength y mi nuevo end-start
-// Si mi nuevo end-start es menor a mi current minLength, se asignara ese valor a mi current minLength
+  // [1,2,3,4,5,6], 4
+  //  i j
+  //   3   is less than sum so I move j
+  //  i  j
+  //   6   is more than sum, store arr.length(j + 1 - i) and move i
 
-let start = 0;
-let end = 0; // Have the responsatbility to accumulate the total sum of my sub array
-let acc = 0;
-let minLength = Infinity
+  let i = 0;
+  let j = 1;
+  let minLength = Infinity
+  let currentSum =nums[i] + nums[j]
 
-while(start < nums.length) {
-  if(end < nums.length && acc < sum) {
-    acc += nums[end]
-    end ++
-    //  [1 + 2 + 3 + 4 ] = 10 // first window
-  } else if(acc >= sum){// search other window
-    minLength = Math.min(minLength, end - start) // 2
-    acc = acc - nums[start] // [  3 + 4 ] 
-    start ++ // [3 + 4 + 5]
-  } else {
-    break
-  } // to stop my algorithms need two conditions: first end < nums.length and then acc < sum. If acc is always < sum, it doesn't matters if my end > arr.length, the loop is gonna be executed for ever
+  while(j < nums.length && i < j) {
+    if(currentSum < sum) {
+      j ++
+      currentSum += nums[j]
+    } else if(currentSum > sum) {
+      minLength = Math.min((j + 1 - i), minLength)
+      currentSum -= nums[i]
+      i ++
+    } else {
+      return
+    }
+  }
+
+  return minLength
 }
 
-return console.log(minLength === Infinity ? 0 : minLength)
-
-}
-
-minSubArrayLen([2,3,1,2,4,3], 7) // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2,3,1,2,4,3], 7)) // 2 -> because [4,3] is the smallest subarray
 minSubArrayLen([2,1,6,5,4], 9) // 2 -> because [5,4] is the smallest subarray
 minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 -> because [62] is greater than 52
 minSubArrayLen([1,4,16,22,5,7,8,9,10],39) // 3
