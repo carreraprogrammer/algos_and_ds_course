621c38baa36436eba52c3b17750ada8a46b30a26
Daniel <carreraprogrammer@gmail.com>
2023-11-12 09:43:40 -0500

    Refactor bubble sort algorithm to improve performance

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,24 @@
 def bubble_sort(arr)
-  length = arr.length
+  n = arr.length
 
-  (0 ... length - 1).each do |i|
-    (0 ... length - i - 1).each do |j|
-      if arr[j] > arr[j + 1]
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  while n > 0 do
+    n -= 1
+    make_switch = false
+    
+    n.times do | i |
+      if arr[i] > arr[i + 1]
+        temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+        make_switch = true
       end
     end
+    print arr
+    break unless make_switch
   end
 
   arr
 end
+
 
 p bubble_sort([1, 4, 3, 2, 5])
