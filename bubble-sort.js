f567e60b5f8d22040330df175f1bfe88b3a543ef
Daniel <carreraprogrammer@gmail.com>
2023-11-12 09:43:49 -0500

    Refactor bubble sort algorithm.

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,12 +1,29 @@
 let bubbleSort = (arr) => {
-  for(let i = arr.length - 1; i >= 0; i ++) {
-    for(let j = 0; j < i -1; j ++) {
-      if(arr[j] > arr[j+1]){
-        [arr[j], arr[j + 1]] = [arr[j+1], arr[j]]
-      }
+// [1, 2, 3, 4, 5, 6]
+//              i  j
+//        i j
+//              i  j
+//  if the number i is more than number j, switch
+// if it is not, i ++ j ++
+// if there is not any switch, we break the function
+
+let makeSwitch;
+
+for(let i = arr.length; i > 0; i --) {
+  makeSwitch = false;
+  console.log(arr)
+
+  for(let j = 0; j < i - 1; j ++) {
+    if(arr[j] > arr[j + 1]) {
+      [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      makeSwitch = true;
     }
   }
-  return arr
+
+  if(!makeSwitch) break;
+}
+
+ return arr
 }
 
 
