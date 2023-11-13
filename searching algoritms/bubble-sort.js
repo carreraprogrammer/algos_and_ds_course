a2f9aa29869bad62a0d0d1bddce4d1934e585c5c
Daniel <carreraprogrammer@gmail.com>
2023-11-13 08:46:33 -0500

    Add explained solution

--- searching algoritms/bubble-sort.js
+++ searching algoritms/bubble-sort.js
@@ -1,30 +1,20 @@
 let bubbleSort = (arr) => {
-// [1, 2, 3, 4, 5, 6]
-//              i  j
-//        i j
-//              i  j
-//  if the number i is more than number j, switch
-// if it is not, i ++ j ++
-// if there is not any switch, we break the function
-
-let makeSwitch;
-
-for(let i = arr.length; i > 0; i --) {
-  makeSwitch = false;
-  console.log(arr)
-
-  for(let j = 0; j < i - 1; j ++) {
-    if(arr[j] > arr[j + 1]) {
-      [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
-      makeSwitch = true;
+  for(let i = arr.length; i > 0; i --) {
+    console.log(arr)
+    let makeSwitch = false;
+    for(let j = 0; j < i; j ++) {
+      if(arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        makeSwitch = true
+      }
     }
+
+    if(!makeSwitch) break;
+    
   }
-
-  if(!makeSwitch) break;
+  return arr
 }
 
- return arr
-}
-
+// [1, 3, 2, 4, 5]
 
 console.log(bubbleSort([1,4,3,2,5,8,9,7]))
--- searching algoritms/selection-sort.js
+++ searching algoritms/selection-sort.js
@@ -1,16 +1,26 @@
 let selectionSort = (arr) => {
-  for(let i = 0; i < arr.length; i ++) {
-    minPosition = i
-    for(let j = i + 1; j < arr.length; j ++) {
-      if(arr[j] < arr[minPosition]) {
-        minPosition = j
-      }
-    }
+  // [1, 3, 5, 2, 4, 6]]
+  //  define a min value arr[0]
+  // compare that first value to find the minimum number in the array
+  // if the min value != firstNumber, switch
+  // In every passing, I can guarantize that the first number is the smallest number, so I can do the next passing starting from the next number
+  let minValue = 0;
 
-    if(minPosition !== i) {
-      [arr[minPosition], arr[i]] = [arr[i], arr[minPosition]]
+  for(let i = 0; i < arr.length; i ++) {
+    minValue = i
+    console.log(arr, minValue)
+   for(let j = i + 1; j < arr.length; j ++) {
+    console.log(arr[minValue])
+    console.log('VS')
+    console.log(arr[j])
+    if(arr[minValue] > arr[j]) {
+      console.log(`switch ${arr[minValue]} by ${arr[j]}`)
+      minValue = j
     }
+   }
+    [arr[i], arr[minValue]] = [arr[minValue], arr[i]]
   }
+
   return arr
 }
 
