eacf2f9223f57d6c1ab101351cfa86b2c50b721e
Daniel <carreraprogrammer@gmail.com>
2023-11-15 09:52:43 -0500

    Refactor selectionSort function to improve efficiency

--- searching algoritms/selection-sort.js
+++ searching algoritms/selection-sort.js
@@ -1,26 +1,20 @@
 let selectionSort = (arr) => {
-  // [1, 3, 5, 2, 4, 6]]
-  //  define a min value arr[0]
-  // compare that first value to find the minimum number in the array
-  // if the min value != firstNumber, switch
-  // In every passing, I can guarantize that the first number is the smallest number, so I can do the next passing starting from the next number
-  let minValue = 0;
-
+// [1, 3, 2, 4]
+//        m     // if arr[0] !== arr[m], switch
+//          i
   for(let i = 0; i < arr.length; i ++) {
-    minValue = i
-    console.log(arr, minValue)
-   for(let j = i + 1; j < arr.length; j ++) {
-    console.log(arr[minValue])
-    console.log('VS')
-    console.log(arr[j])
-    if(arr[minValue] > arr[j]) {
-      console.log(`switch ${arr[minValue]} by ${arr[j]}`)
-      minValue = j
-    }
-   }
-    [arr[i], arr[minValue]] = [arr[minValue], arr[i]]
-  }
+    let minNumber = i
 
+    for(let j = i + 1; j < arr.length; j ++) {
+      if(arr[j] < arr[minNumber]) {
+        minNumber = j
+      }
+    }
+
+    if(arr[i] !== arr[minNumber]) {
+      [arr[i], arr[minNumber]] = [arr[minNumber], arr[i]]
+    }
+  }
   return arr
 }
 
